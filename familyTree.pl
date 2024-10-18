35990be88bd0d85bd12980e135dc909eed2e237e
Shivam Prajapati <137375245+Shivamprajapatii@users.noreply.github.com>
2024-10-18 07:24:43 +0530

    Update familyTree.pl
    how to Excute

--- familyTree.pl
+++ familyTree.pl
@@ -23,3 +23,9 @@ grandmother(X,Z):-mother(X,Y),parent(Y,Z).
 grandfather(X,Z):-father(X,Y),parent(Y,Z).
 wife(X,Y):-parent(X,Z),parent(Y,Z),female(X),male(Y).
 uncle(X,Z):-brother(X,Y),parent(Y,Z).
+
+
+op
+sister(ann,peter).
+
+wife(pam,tom)
